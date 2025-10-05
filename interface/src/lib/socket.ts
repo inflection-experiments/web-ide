2859b1d50e6067d9a5925fa649335732bc29d7e2
SAMARTH MUKTAMATH <samarthmise2025@gmail.com>
2025-10-05 20:48:10 +0530

    .env added

--- interface/src/lib/socket.ts
+++ interface/src/lib/socket.ts
@@ -1,6 +1,7 @@
 // src/lib/socket.ts
 import { io, Socket } from 'socket.io-client';
+import { env } from '$env/dynamic/public';
 
-const socket: Socket = io('http://localhost:9000');
+const socket: Socket = io(env.PUBLIC_SOCKET_URL);
 
 export default socket;
